b0a9e8b1a461469e41188c7952934072c5171f95
Steventan05 <70800912+Steventan05@users.noreply.github.com>
2025-12-14 15:36:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/home', function () {
     return view('home');
 });
+
+Route::get('/study-share', function () {
+    return view('study-share');
+});
